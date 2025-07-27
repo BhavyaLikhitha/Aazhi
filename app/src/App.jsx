7f07bbb3ace3e61b5752ce9a539e457c2beedd77
bhavyalikhitha7 <bukka.b@northeastern.edu>
2025-07-27 10:29:46 -0400

    Navabar created

---
+++ app/src/App.jsx
@@ -0,0 +1,17 @@
+import HomePage from './Components/Homepage/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
---
+++ app/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+// src/components/Navbar.jsx
+
+import React from "react";
+import "./Navbar.css";
+import logo from './logo.png'
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__left">
+        <img src={logo} alt="Aazhi Logo" className="navbar__logo" />
+        <div className="navbar__brand">
+          <h1 className="navbar__title">Aazhi</h1>
+          <p className="navbar__subtitle">Mental Health services</p>
+        </div>
+      </div>
+
+      <nav className="navbar__links">
+        <a href="#">Home</a>
+        <a href="#">Book a Session</a>
+        <a href="#">Blog</a>
+        <a href="#">About Us</a>
+      </nav>
+
+    </header>
+  );
+}
+
+export default Navbar;
