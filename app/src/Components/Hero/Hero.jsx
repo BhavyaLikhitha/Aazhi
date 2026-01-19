b006c4ae96e87c09f41f55c0760668ac587f2cdd
Bhavyalikhitha <bhavyalikhitha.bbl@gmail.com>
2026-01-19 12:55:32 -0500

    hero section

---
+++ app/src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+// src/components/HeroSection.jsx
+
+import React from "react";
+import "./hero.css";
+import waveImage from "./hero.png"; // <- replace with your actual image filename
+
+function HeroSection() {
+  return (
+    <section className="hero">
+      <div className="hero__left">
+        <img src={waveImage} alt="Wave" className="hero__wave" />
+      </div>
+
+      <div className="hero__right">
+        <h2 className="hero__quote">
+          "YOU DON'T HAVE TO SAIL THROUGH THIS ALONE <br />
+          LET’S NAVIGATE IT TOGETHER."
+        </h2>
+       <button className="hero__button">Book a session</button>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
