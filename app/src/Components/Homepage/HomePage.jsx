0833a798b11057321991a4b1ff621b92e9646c63
Bhavyalikhitha <bhavyalikhitha.bbl@gmail.com>
2026-01-19 12:56:14 -0500

    added hero componenet

--- app/src/Components/Homepage/HomePage.jsx
+++ app/src/Components/Homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/Hero'
 // import Footer from '../Footer/Footer'
 
 function HomePage() {
     return (
         <>
             <Navbar />
+            <Hero />
               {/* <Footer /> */}
         </>
     );
