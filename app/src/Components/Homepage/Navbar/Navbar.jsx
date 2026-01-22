e022450d2c9c2f4f5cbc39200e87c675c09dae4c
Bhavyalikhitha <bhavyalikhitha.bbl@gmail.com>
2026-01-21 22:23:22 -0500

    blogs section

--- app/src/Components/Homepage/Navbar/Navbar.jsx
+++ app/src/Components/Homepage/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
       <nav className="navbar__links">
   <a href="/" className="nav-btn">Home</a>
   <a href="/book-session" className="nav-btn">Book Online Session</a>
-  <a href="/blogs" className="nav-btn">Blog</a>
+  <a href="/blogs" className="nav-btn">Blogs</a>
   <a href="/products" className="nav-btn">Products</a>
   <a href="/about-us" className="nav-btn">About Us</a>
 </nav>
